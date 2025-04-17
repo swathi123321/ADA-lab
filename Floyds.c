0cf0d5a9689c6fa730e43a5e8edfd8af61e4e9a0
swathi123321 <swathi.23cs172@sode-edu.in>
2025-04-17 15:58:16 +0530

    Add files via upload

---
+++ Floyds.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#define MAX 20
+void floyd(int n,int d[MAX][MAX],int W[MAX][MAX]);
+int min(int a,int b);
+int main()
+{
+int d[MAX][MAX];
+int W[MAX][MAX];
+int i,j,n;
+printf("enter the number of nodes:");
+scanf("%d",&n);
+printf("enter the weight:\n");
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n;j++)
+{
+scanf("%d",&W[i][j]);
+}
+}
+floyd(n,d,W);
+return 0;
+}
+void floyd(int n,int d[MAX][MAX],int W[MAX][MAX]){
+int i,j,k;
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n;j++)
+{
+d[i][j]=W[i][j];
+}
+}
+for(k=1;k<=n;k++)
+{
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n;j++)
+{
+d[i][j]=min(d[i][j],d[i][k]+d[k][j]);
+}
+}
+}
+printf("All pair shortest path matrix is:\n");
+for(i=1;i<=n;i++)
+{
+printf("\t%d",i);
+}
+printf("\n\t................\n");
+for(i=1;i<=n;i++)
+{
+printf("%d\t",i);
+for(j=1;j<=n;j++)
+{
+printf("%d\t",d[i][j]);
+}
+printf("\n");
+}
+}
+int min(int a,int b){
+return (a<b)?a:b;
+}
